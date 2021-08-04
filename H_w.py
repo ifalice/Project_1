2e6e1f4c089dcd78cee73e01eebe8e3b9703c70e
IFAlice <mr.spankibaster@gmail.com>
2021-08-04 23:17:42 +0300

    Add new class Person

--- H_w.py
+++ H_w.py
@@ -1,1 +1,14 @@
-print('Hello world')
+class Person:
+    
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def call(self):
+        return self.__name
+
+a = Person("Yura")
+print(a.call)
+
+
+        
