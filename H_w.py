fa650cfc7de53a55fb72603d29dd528933a72191
IFAlice <mr.spankibaster@gmail.com>
2021-08-05 23:19:50 +0300

    Add new H_w.py

--- H_w.py
+++ H_w.py
@@ -1,1 +1,22 @@
-Prifdfsdfsdfsdfsdfs
+class Person:
+    
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def call(self):
+        return self.__name
+
+    @call.setter
+    def call(self, new_name):
+        self.name = new_name
+
+    @call.deleter
+    def call(self):
+        del self.name
+
+a = Person("Yura")
+print(a.call)
+b = Person("Misha")
+
+        
