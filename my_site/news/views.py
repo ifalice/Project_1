63584c342248e267c1b0f560d28784de57498eba
IFAlice <mr.spankibaster@gmail.com>
2021-08-07 19:36:12 +0300

    add my_site and app news

---
+++ my_site/news/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    # print(dir(request))
+    return HttpResponse("Hello world")
+
+def test(request):
+    return HttpResponse("<h1>Test store</h1>")
+
+
+
