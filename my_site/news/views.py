c33945fc5e8a7186c57f049a4cd7ede201989ef8
IFAlice <mr.spankibaster@gmail.com>
2021-08-12 22:08:08 +0300

    add tamplatetags and news_tags.py. Add tag get_categories and show_categories

--- my_site/news/views.py
+++ my_site/news/views.py
@@ -6,18 +6,15 @@ from .models import Category, News
 
 def index(request):
     news = News.objects.all()
-    categories = Category.objects.all()
     context = {
         'news': news,
-        'title': "Список новостей",
-        'categories': categories
+        'title': "Список новостей",        
     }
     return render(request, 'news/index.html', context)
 
 
 def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
+    news = News.objects.filter(category_id=category_id) 
     category = Category.objects.get(pk = category_id)
-    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
+    return render(request, 'news/category.html', {'news': news, 'category': category})
  
